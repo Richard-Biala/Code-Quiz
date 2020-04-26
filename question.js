0e4c5e3bdb5ba8466fb5b84c92c44b8a540730d0
Richard <rkj128@gmail.com>
2020-04-25 22:55:24 -0700

    Code Quiz

---
+++ question.js
@@ -0,0 +1,91 @@
+function Quiz(questions) {
+  this.score = 0;
+  this.questions = questions;
+  this.questionIndex = 0;
+}
+
+Quiz.prototype.getQuestionIndex = function() {
+  return this.questions[this.questionIndex];
+}
+
+Quiz.prototype.guess = function(answer) {
+  if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+      this.score++;
+  }
+
+  this.questionIndex++;
+}
+
+Quiz.prototype.isEnded = function() {
+  return this.questionIndex === this.questions.length;
+}
+
+
+function Question(text, choices, answer) {
+  this.text = text;
+  this.choices = choices;
+  this.answer = answer;
+}
+
+Question.prototype.isCorrectAnswer = function(choice) {
+  return this.answer === choice;
+}
+
+
+function populate() {
+  if(quiz.isEnded()) {
+      showScores();
+  }
+  else {
+      // show question
+      var element = document.getElementById("question");
+      element.innerHTML = quiz.getQuestionIndex().text;
+
+      // show options
+      var choices = quiz.getQuestionIndex().choices;
+      for(var i = 0; i < choices.length; i++) {
+          var element = document.getElementById("choice" + i);
+          element.innerHTML = choices[i];
+          guess("btn" + i, choices[i]);
+      }
+
+      showProgress();
+  }
+};
+
+function guess(id, guess) {
+  var button = document.getElementById(id);
+  button.onclick = function() {
+      quiz.guess(guess);
+      populate();
+  }
+};
+
+
+function showProgress() {
+  var currentQuestionNumber = quiz.questionIndex + 1;
+  var element = document.getElementById("progress");
+  element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+
+function showScores() {
+  var gameOverHTML = "<h1>Result</h1>";
+  gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+  var element = document.getElementById("quiz");
+  element.innerHTML = gameOverHTML;
+};
+
+// create questions here
+var questions = [
+  new Question("What color is the sky?", ["Blue", "Green","Red", "Purple"], "Blue"),
+  new Question("What color is the sky at night?", ["Black", "Red", "Green", "Purple"], "Black"),
+  new Question("What color is the sky on sunrise?", ["Red", "Blue","Black", "Orange"], "Orange"),
+  new Question("What color is a is an Orange?", ["Yellow", "Red", "Orange", "Purple"], "Orange"),
+  new Question("Guess what color my car is?", ["Red", "Black", "White", "Silver"], "Black")
+];
+
+// create quiz
+var quiz = new Quiz(questions);
+
+// display quiz
+populate();
